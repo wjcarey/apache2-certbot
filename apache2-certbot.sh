b58adec8ee9a078b02a811f2d61d6e5a6ca4ccbb
WJ Carey <contact@wjcarey.io>
2020-10-16 14:37:58 -0600

    changed variable names

--- apache2-certbot.sh
+++ apache2-certbot.sh
@@ -3,16 +3,18 @@
 if [ -z "$1" ]
     then
         echo "enter the webroot of your website ..."
-        read WEB_ROOT
+        read USER_PATH
         echo "enter the domain name of your website ..."
-        read INSTALL_DOMAIN
+        read DOMAIN_NAME
     else
-    WEB_ROOT=${1}
-    INSTALL_DOMAIN=${2}
+    USER_PATH=${1}
+    DOMAIN_NAME=${2}
 fi
 
+INSTALL_PATH=$(realpath -s --canonicalize-missing $USER_PATH)
+
 sudo apt-get install certbot python3-certbot-apache -y
-certbot -a webroot -i apache -w ${WEB_ROOT} -d ${INSTALL_DOMAIN} --agree-tos --no-eff-email --redirect --register-unsafely-without-email
+certbot -a webroot -i apache -w ${INSTALL_PATH} -d ${INSTALL_DOMAIN} --agree-tos --no-eff-email --redirect --register-unsafely-without-email
 certbot renew --dry-run
 
 #SELF DELETE AND EXIT
