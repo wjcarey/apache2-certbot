32cbe441e9fda9822cb4cbbca47f6bb987e5d1c4
WJ Carey <contact@wjcarey.io>
2020-10-19 10:20:23 -0600

    addded redelacred variables

--- apache2-certbot.sh
+++ apache2-certbot.sh
@@ -3,15 +3,15 @@
 if [ -z "$1" ]
     then
         echo "enter the webroot of your website ..."
-        read USER_PATH
+        read INSTALL_PATH
         echo "enter the domain name of your website ..."
         read DOMAIN_NAME
     else
-    USER_PATH=${1}
+    INSTALL_PATH=${1}
     DOMAIN_NAME=${2}
 fi
 
-INSTALL_PATH=$(realpath -s --canonicalize-missing $USER_PATH)
+INSTALL_PATH=$(realpath -s --canonicalize-missing $INSTALL_PATH)
 
 sudo apt-get install certbot python3-certbot-apache -y
 certbot -a webroot -i apache -w ${INSTALL_PATH} -d ${INSTALL_DOMAIN} --agree-tos --no-eff-email --redirect --register-unsafely-without-email
